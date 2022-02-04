6d29b109ce445f130ccdecb90c37a8d096999495
Rufus Pitt <57277756+RufusPitt@users.noreply.github.com>
2022-02-04 14:04:41 +0000

    Update index.js
    Hello world test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Section')
+  res.send('Hello World')
 })
 
 app.listen(port, () => {
