ec3acfb859a66fee3895bdbb20cc44cc44461a74
Jack Rive <jack.rive2@gmail.com>
2022-03-07 19:12:05 +0000

    updated user and users api's for new user table

--- routes/user.js
+++ routes/user.js
@@ -30,6 +30,21 @@ router.post("/", (req, res, next) => { //restrict only to post commands
     if (!req.body.username){
         errors.push("No username specified");
     }
+    if (!req.body.rank){
+        errors.push("No rank specified");
+    }
+    if (!req.body.total_kills){
+        errors.push("No total_kills specified");
+    }
+    if (!req.body.total_deaths){
+        errors.push("No total_deaths specified");
+    }
+    if (!req.body.total_won){
+        errors.push("No total_won specified");
+    }
+    if (!req.body.total_lost){
+        errors.push("No total_lost specified");
+    }
     if (errors.length){
         res.status(400).json({"error":errors.join(",")});
         return;
@@ -37,10 +52,15 @@ router.post("/", (req, res, next) => { //restrict only to post commands
     var data = {
         steamID: req.body.steamID,
         username: req.body.username,
+        rank: req.body.rank,
+        total_kills: req.body.total_kills,
+        total_deaths: req.body.total_deaths,
+        total_won: req.body.total_won,
+        total_lost: req.body.total_lost
         //password : md5(req.body.password)
     }
-    var sql ='INSERT INTO user (steamID, username) VALUES (?,?)'
-    var params = [data.steamID, data.username]
+    var sql ='INSERT INTO user (steamID, username, rank, total_kills, total_deaths, total_won, total_lost) VALUES (?,?,?,?,?,?,?)'
+    var params = [data.steamID, data.username, data.rank, data.total_kills, data.total_kills, data.total_won, data.total_lost]
     db.run(sql, params, function (err, result) {
         if (err){
             res.status(400).json({"error": err.message})
